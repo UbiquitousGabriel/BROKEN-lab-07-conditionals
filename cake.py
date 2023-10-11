75e8567d1f0d04d2453810cc3452d9533284349e
Gabriel Aguilar <ubiquitousgabriel@gmail.com>
2023-10-11 19:27:21 -0400

    +IDK Option

--- cake.py
+++ cake.py
@@ -1,5 +1,7 @@
-prompt("Do you want some cake?")
-            if input = ("yes")
-                print("Have some cake.")
-            else 
-                print("No cake for you.")
+cakeorno = input("Do you want some cake?")
+if cakeorno == ("yes"):
+    print("Have some cake.")
+elif cakeorno == "no": 
+    print("No cake for you.")
+else: 
+    print("I don't understand.")
